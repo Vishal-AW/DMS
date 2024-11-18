04c13f5a49c5d59981fe1d2476682202d949176a
Priyankakamble3103 <priyanka@ascenwork.com>
2024-11-18 13:46:56 +0530

    update master1

--- src/webparts/homePage/components/Master/Master.tsx
+++ src/webparts/homePage/components/Master/Master.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
+import styles from '../Master/Master.module.scss';
 
 export default function Master():JSX.Element{
 
 return(
 
-    <div>
+    <div className={styles.h1}>
         Master Data File 123
     </div>
 )
