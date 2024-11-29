54513ea701875272ff3afe63d88a7bde911a339c
Priyankakamble3103 <priyanka@ascenwork.com>
2024-11-29 10:27:22 +0530

    Update submenu

--- src/webparts/homePage/components/Route/SideMenu.tsx
+++ src/webparts/homePage/components/Route/SideMenu.tsx
@@ -31,7 +31,7 @@ const SideMenu: React.FC<ISideMenu> = ({ onclickbutton }) => {
 
     <div style={{ display: 'flex', height: '100vh' }}>
       <Sidebar collapsed={collapsed} style={{
-        color: '#fff',         // Text color
+        color: '#fff',// Text color
       }}
       >
 
@@ -50,19 +50,19 @@ const SideMenu: React.FC<ISideMenu> = ({ onclickbutton }) => {
         <Menu>
 
           <SubMenu icon={<Icon iconName="BarChartVertical" style={{ color: '#3f4254', fontSize: '18px' }} />} label="Charts">
-            <MenuItem>Pie charts</MenuItem>
-            <MenuItem>Line charts</MenuItem>
-            <MenuItem>Bar charts</MenuItem>
+            <MenuItem icon={<Icon iconName="LocationDot" style={{ color: '#b5b5c3', fontSize: '13px' }} />}>Pie charts</MenuItem>
+            <MenuItem icon={<Icon iconName="LocationDot" style={{ color: '#b5b5c3', fontSize: '13px' }} />}>Line charts</MenuItem>
+            <MenuItem icon={<Icon iconName="LocationDot" style={{ color: '#b5b5c3', fontSize: '13px' }} />}>Bar charts</MenuItem>
           </SubMenu>
 
           <SubMenu icon={<Icon iconName="Globe" style={{ color: '#3f4254', fontSize: '18px' }} />} label="Maps">
-            <MenuItem>Google maps</MenuItem>
-            <MenuItem>Open street maps</MenuItem>
+            <MenuItem icon={<Icon iconName="LocationDot" style={{ color: '#b5b5c3', fontSize: '13px' }} />}>Google maps</MenuItem>
+            <MenuItem icon={<Icon iconName="LocationDot" style={{ color: '#b5b5c3', fontSize: '13px' }} />}>Open street maps</MenuItem>
           </SubMenu>
 
           <SubMenu icon={<Icon iconName="Color" style={{ color: '#3f4254', fontSize: '18px' }} />} label="Theme">
-            <MenuItem>Dark</MenuItem>
-            <MenuItem>Light</MenuItem>
+            <MenuItem icon={<Icon iconName="LocationDot" style={{ color: '#b5b5c3', fontSize: '13px' }} />}>Dark</MenuItem>
+            <MenuItem icon={<Icon iconName="LocationDot" style={{ color: '#b5b5c3', fontSize: '13px' }} />}>Light</MenuItem>
           </SubMenu>
 
           <MenuItem component={<Link to="/" />}
