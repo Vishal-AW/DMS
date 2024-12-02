b9d266a91dde87754f214ee20bf56076be51c050
Priyankakamble3103 <priyanka@ascenwork.com>
2024-12-02 19:27:07 +0530

    update02

--- src/webparts/homePage/components/Constants/Constants.ts
+++ src/webparts/homePage/components/Constants/Constants.ts
@@ -2,21 +2,21 @@
 
 export class ConstName {
 
-public static readonly Const_Route={
-    Dashboard : "Dashboard",
-    Master : "/Master"
-}
+    public static readonly Const_Route = {
+        Dashboard: "Dashboard",
+        Master: "/Master"
+    }
 
 
-public static readonly Const_ListName={
-    Dashboard : "Dashboard",
-    Master : "/Master"
-}
+    public static readonly Const_ListName = {
+        Dashboard: "Dashboard",
+        Master: "/Master"
+    }
 
 
 
 
- 
+
 
 
 }
