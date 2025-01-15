015d515b72bfa8744b605678a77138190e982029
snehaghule30 <sneha@ascenwork.com>
2025-01-15 19:52:56 +0530

    SearchCodeUpdate

--- src/webparts/homePage/components/GeneralDocumentTreeView/UploadFile.tsx
+++ src/webparts/homePage/components/GeneralDocumentTreeView/UploadFile.tsx
@@ -34,17 +34,17 @@ function UploadFiles({ context, isOpenUploadPanel, dismissUploadPanel, folderPat
     const [isUpdateExistingFile, setIsUpdateExistingFile] = useState<boolean>(false);
     const [isPopupBoxVisible, setIsPopupBoxVisible] = useState<boolean>(false);
 
-    const peoplePickerContext: IPeoplePickerContext = {
-        absoluteUrl: context.pageContext.web.absoluteUrl,
-        msGraphClientFactory: context.msGraphClientFactory,
-        spHttpClient: context.spHttpClient
-    };
-
     // const peoplePickerContext: IPeoplePickerContext = {
     //     absoluteUrl: context.pageContext.web.absoluteUrl,
-    //     msGraphClientFactory: context.msGraphClientFactory as any as import("@pnp/spfx-controls-react/node_modules/@microsoft/sp-http-msgraph/dist/index-internal").MSGraphClientFactory,
-    //     spHttpClient: context.spHttpClient as any as import("@pnp/spfx-controls-react/node_modules/@microsoft/sp-http-base/dist/index-internal").SPHttpClient
+    //     msGraphClientFactory: context.msGraphClientFactory,
+    //     spHttpClient: context.spHttpClient
     // };
+
+    const peoplePickerContext: IPeoplePickerContext = {
+        absoluteUrl: context.pageContext.web.absoluteUrl,
+        msGraphClientFactory: context.msGraphClientFactory as any as import("@pnp/spfx-controls-react/node_modules/@microsoft/sp-http-msgraph/dist/index-internal").MSGraphClientFactory,
+        spHttpClient: context.spHttpClient as any as import("@pnp/spfx-controls-react/node_modules/@microsoft/sp-http-base/dist/index-internal").SPHttpClient
+    };
 
 
 
